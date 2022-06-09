393c0411a10ea65e2293c743f206992caaf98a7d
Saifullah Mansur <58740153+saikot-cse@users.noreply.github.com>
2022-06-09 23:36:35 +0600

    pic uploadrd

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-
+import profile from "../public/saikot.JPG"
 const About = () => {
   return (
     <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
@@ -12,20 +12,10 @@ const About = () => {
           <h2 className='py-4'>Who I Am</h2>
           
           <p className='py-2 text-gray-600'>
-            I have spent the last 12 years in the fire service working as a
-            professional firefighter & paramedic. I have always had a knack for
-            technology and working with computers. In 2019 I started working
-            with HTML & CSS to make some minor edits on a small business website
-            that I was operating. What I thought was just a few small edits
-            turned into a love for programming.
+          I&apos;ve been studying web programming for the past 12 months in order to become a professional web developer. I&apos;ve always enjoyed working with computers and technology. In 2019, I began using HTML and CSS to make tiny changes to a small business website that I was managing. What started off as a few little changes developed into a passion for programming.
           </p>
           <p className='py-2 text-gray-600'>
-            Fascinated with how intricate programming can be I was quickly drawn
-            to learn more. I started learning javascript and was even more
-            enthused with making websites interactive. I then started
-            freelancing for e-commerce companies on the Shopify platform. I am
-            now spending my time building projects with React JS, Firebase, and
-            learning new technologies.
+          I was immediately drawn to learn more about programming because of how complex it can be. I began learning javascript and became even more enthusiastic about creating interactive websites. After that, I enrolled in Jhankar Mahbub&apos;s web development course. Now I spend my time exploring new technologies and building applications with React JS, Firebase, and MongoDB.
           </p>
           <a href='/#projects' target="_blank" rel='norefferer'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
@@ -36,7 +26,7 @@ const About = () => {
         <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
           <img
             className='rounded-xl'
-            src='https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80'
+            src={profile}
             alt='/'
           />
         </div>
